7b11d7a915dc0f95f3378c4e36a7e19b1dab1e35
Difusão Web <difusaowebinc@gmail.com>
2022-08-18 12:43:11 -0300

    Controller list and delete prepared.

--- app/Controllers/Http/ServicesController.ts
+++ app/Controllers/Http/ServicesController.ts
@@ -63,30 +63,6 @@ export default class ServicesController {
     }
   }
 
-  public async delete ({ request, response }: HttpContextContract) {
-    try {
-      const qs = request.qs()
-      if (!qs.service_ids) {
-        response.send({ failure: { message: 'lack of data' } })
-        response.status(500)
-        return response
-      }
-
-      const serviceIds: number[] = qs.service_ids
-
-      await Database.from('services').delete().whereIn('id', serviceIds)
-
-      response.send({ success: true })
-      response.status(200)
-      return response
-    } catch (err) {
-      console.log(err)
-      response.send({ failure: { message: 'lack of data' } })
-      response.status(500)
-      return response
-    }
-  }
-
   public async list ({ request, response }: HttpContextContract) {
     try {
       const qs = request.qs()
@@ -107,6 +83,7 @@ export default class ServicesController {
       )
         .leftJoin('attachment_service', 'attachment_service.service_id', '=', 'services.id')
         .leftJoin('attachments', 'attachments.id', '=', 'attachment_service.attachment_id')
+        .orderBy('created_at', 'desc')
         .paginate(page, perPage)
 
       if (responseDb.all().length === 0) {
@@ -127,6 +104,40 @@ export default class ServicesController {
     } catch (err) {
       console.log(err)
       response.send({ failure: { message: 'Error getting service list.' } })
+      response.status(500)
+      return response
+    }
+  }
+
+  public async delete ({ request, response }: HttpContextContract) {
+    try {
+      const qs = request.qs()
+      if (!qs.services_id) {
+        response.send({ failure: { message: 'Lack of data.' } })
+        response.status(500)
+        return response
+      }
+      const servicesId: number[] = qs.services_id
+
+      const responseDb = await Database.from('services').select('id').whereIn('id', servicesId)
+      if (responseDb.length === 0) {
+        response.send({ failure: { message: 'Services not found.' } })
+        response.status(404)
+        return response
+      }
+
+      await Database.from('services').whereIn('id', servicesId).delete()
+
+      response.send({
+        success: {
+          deleted: true
+        }
+      })
+      response.status(200)
+      return response
+    } catch (err) {
+      console.log(err)
+      response.send({ failure: { message: 'Error when deleting the services.' } })
       response.status(500)
       return response
     }
