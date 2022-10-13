5d7fbd03fdc74ddd456e92bafbf9cce2bb8a8777
Difusão Web <difusaowebinc@gmail.com>
2022-10-13 06:45:15 -0300

    The Services route module has been updated.

--- app/Modules/Services/routes.ts
+++ app/Modules/Services/routes.ts
@@ -2,6 +2,7 @@ import Route from '@ioc:Adonis/Core/Route'
 Route.group(() => {
   Route.group(() => {
     Route.get('/list', 'ServicesController.list')
+    Route.get('/show', 'ServicesController.show')
     Route.group(() => {
       Route.get('/delete', 'ServicesController.delete')
       Route.get('/create', 'ServicesController.create')
